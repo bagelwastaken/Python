bc0d8564d0891a2c9ec92cc53dbd1407c4d39320
Mathusan.P <78277278+bagelwastaken@users.noreply.github.com>
2021-07-19 05:32:15 +0100

    Add files via upload
    Added a dice

---
+++ Dice.py
@@ -0,0 +1,12 @@
+import random
+import time
+#Here we import 2 modules in python, random and time
+
+dice = random.randrange(1,6)
+#I declare dice as a variable and I set it so the random number to be generated should be between 1 and 6
+print(dice)
+#I then print the variable 
+time.sleep(5)
+#I then pause the code for 5 seconds using the time module
+
+
